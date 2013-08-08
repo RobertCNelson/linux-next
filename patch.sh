0fec6f90237624ad85899e43d433c65b918f53a8
Robert Nelson <robertcnelson@gmail.com>
2013-08-08 12:38:26 -0500

    init repo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ patch.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+#
+# Copyright (c) 2009-2013 Robert Nelson <robertcnelson@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Split out, so build_kernel.sh and build_deb.sh can share..
+
+git="git am"
+
+git_patchset="git://git.kernel.org/pub/scm/linux/kernel/git/arm/arm-soc.git"
+if [ -f ${DIR}/system.sh ] ; then
+	. ${DIR}/system.sh
+	if [ "${GIT_OVER_HTTP}" ] ; then
+		git_patchset="http://git.kernel.org/pub/scm/linux/kernel/git/arm/arm-soc.git"
+	fi
+fi
+
+if [ "${RUN_BISECT}" ] ; then
+	git="git apply"
+fi
+
+echo "Starting patch.sh"
+
+git_add () {
+	git add .
+	git commit -a -m 'testing patchset'
+}
+
+start_cleanup () {
+	git="git am --whitespace=fix"
+}
+
+cleanup () {
+	if [ "${number}" ] ; then
+		git format-patch -${number} -o ${DIR}/patches/
+	fi
+	exit
+}
+
+arm_soc () {
+	tag="for-next"
+	echo "pulling: ${tag}"
+	git pull ${GIT_OPTS} ${git_patchset} ${tag}
+}
+
+arm_soc
+
+echo "patch.sh ran successful"
---
+++ version.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+#Dual/Quad Core arms are now more prevalent, so just don't limit it x86:
+check_cpuinfo=$(cat /proc/cpuinfo | grep "^processor" | awk '{print $1}' | head -n 1)
+if [ "x${check_cpuinfo}" = "xprocessor" ] ; then
+	CORES=$(cat /proc/cpuinfo | grep "^processor" | wc -l)
+else
+	CORES=1
+fi
+
+unset GIT_OPTS
+unset GIT_NOEDIT
+LC_ALL=C git help pull | grep -m 1 -e "--no-edit" >/dev/null 2>&1 && GIT_NOEDIT=1
+
+if [ "${GIT_NOEDIT}" ] ; then
+	GIT_OPTS="${GIT_OPTS} --no-edit"
+fi
+
+config="multi_v7_defconfig"
+
+#linaro_toolchain="arm9_gcc_4_7"
+#linaro_toolchain="cortex_gcc_4_6"
+#linaro_toolchain="cortex_gcc_4_7"
+linaro_toolchain="cortex_gcc_4_8"
+
+#Kernel/Build
+KERNEL_REL=3.11
+KERNEL_TAG=${KERNEL_REL}-rc3
+BUILD=arm-soc
+
+#v3.X-rcX + upto SHA
+#KERNEL_SHA=""
+
+#git branch
+#BRANCH="v3.X.x"
+
+BUILDREV=1.0
+DISTRO=cross
+DEBARCH=armhf
