29254e7c92e99fd835301401935e7e50e8590dca
Robert Nelson <robertcnelson@gmail.com>
2014-06-30 09:06:53 -0500

    kernel bump: v3.16-rc3 + 20140630
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,10 +26,10 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.16
-KERNEL_TAG=${KERNEL_REL}-rc2
+KERNEL_TAG=${KERNEL_REL}-rc3
 BUILD=x0
 
-tag="20140626"
+tag="20140630"
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
