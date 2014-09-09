deeb0dca9acc1925520460246a463274d5ca9f10
Robert Nelson <robertcnelson@gmail.com>
2014-09-09 10:18:59 -0500

    kernel bump: v3.17-rc4 + 20140909
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,10 +26,10 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=3.17
-KERNEL_TAG=${KERNEL_REL}-rc3
+KERNEL_TAG=${KERNEL_REL}-rc4
 BUILD=x0
 
-tag="20140902"
+tag="20140909"
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
