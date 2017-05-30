174e5daaef32a5917dcedacb87b324827c7b2e2e
Robert Nelson <robertcnelson@gmail.com>
2017-05-30 09:29:20 -0500

    kernel bump: v4.12-rc3 + 20170530
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,8 +21,8 @@ KERNEL_ARCH=arm
 #toolchain="gcc_linaro_gnueabihf_4_8"
 #toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
-toolchain="gcc_linaro_gnueabihf_6"
-#toolchain="gcc_linaro_gnueabihf_7"
+#toolchain="gcc_linaro_gnueabihf_6"
+toolchain="gcc_linaro_gnueabihf_7"
 #arm64
 #KERNEL_ARCH=arm64
 #toolchain="gcc_linaro_aarch64_gnu_5"
@@ -30,12 +30,12 @@ toolchain="gcc_linaro_gnueabihf_6"
 #toolchain="gcc_linaro_aarch64_gnu_7"
 
 #Kernel/Build
-KERNEL_REL=4.9
-KERNEL_TAG=${KERNEL_REL}
+KERNEL_REL=4.12
+KERNEL_TAG=${KERNEL_REL}-rc3
 BUILD=${build_prefix}0
 kernel_rt=".X-rtY"
 
-tag="20161215"
+tag="20170530"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
